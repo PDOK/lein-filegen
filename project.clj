8a29763611e96fd726f661e1c572371274c5524d
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-12-19 14:13:56 +0100

    gpg

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject pdok/lein-filegen "0.1.0-SNAPSHOT"
+(defproject pdok/lein-filegen "0.1.0"
   :description "Leiningen plugin to generate files"
+  :signing {:gpg-key "pdok@kadaster.nl"}
   :url "https://github.com/PDOK/lein-filegen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}

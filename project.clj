b3a70b2bf9266652d48387b0f19794b68f9d3c1f
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-12-19 14:27:51 +0100

    simple release

--- project.clj
+++ project.clj
@@ -1,7 +1,16 @@
 (defproject pdok/lein-filegen "0.1.0-SNAPSHOT"
   :description "Leiningen plugin to generate files"
-  :signing {:gpg-key "pdok@kadaster.nl"}
   :url "https://github.com/PDOK/lein-filegen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :username :env
+                              :sign-releases false
+                              :password :env}]]
+  :deploy-repositories [["releases" :clojars]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["deploy"]]
   :eval-in-leiningen true)
